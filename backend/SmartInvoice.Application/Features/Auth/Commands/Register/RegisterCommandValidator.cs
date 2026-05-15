58a7324012b8de848661929d9424ee4b3daff483
TshepoMahloko17 <59024469+TshepoMahloko17@users.noreply.github.com>
2026-05-15 22:37:00 +0200

    Add password strength validation on RegisterCommand (FluentValidation)

---
+++ backend/SmartInvoice.Application/Features/Auth/Commands/Register/RegisterCommandValidator.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+
+namespace SmartInvoice.Application.Features.Auth.Commands.Register;
+
+public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+{
+    public RegisterCommandValidator()
+    {
+        RuleFor(x => x.FirstName).NotEmpty().MaximumLength(100);
+        RuleFor(x => x.LastName).NotEmpty().MaximumLength(100);
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters.")
+            .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+            .Matches("[0-9]").WithMessage("Password must contain at least one number.")
+            .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character.");
+    }
+}
